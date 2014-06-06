127725eb451270898e68eea9cb21e1372cc76414
javi <qualopec@gmail.com>
2014-06-06 15:03:40 +0200

    added example

---
+++ quickexample/test.py
@@ -0,0 +1,8 @@
+import mapnik
+print mapnik.__file__
+mapfile = "def.xml"
+m = mapnik.Map(1400, 600)
+mapnik.load_map(m, mapfile)
+bbox = mapnik.Envelope(mapnik.Coord(-180.0, -75.0), mapnik.Coord(180.0, 90.0))
+m.zoom_to_box(bbox) 
+mapnik.render_to_file(m, 'world_population.png', 'png')
